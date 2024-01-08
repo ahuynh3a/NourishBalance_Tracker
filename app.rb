92201cf53781b03b3b92def5420a7742c1c0e7bb
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-01-08 05:33:22 +0000

    added api url

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+post("/nutrition_facts") do
+  @user_recipie_for_analysis = params.fetch("user_recipie")
+
+  nutrition_analysis_key = ENV.fetch("NUTRITION_ANALYSIS_API")
+
+  nutrition_url =  https://api.edamam.com/api/nutrition-details
+
+  @raw_gmaps_data = HTTP.get(gmaps_url).to_s
+
+  @parsed_response= JSON.parse(@raw_gmaps_data)
+  erb(:nutrition_facts)
 end
